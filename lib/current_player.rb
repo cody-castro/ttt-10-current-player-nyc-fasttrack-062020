290833a72dcbd6f180532fb68dc4f4ecf39f7bfc
Cody C <codyicastro@gmail.com>
2020-06-24 00:39:36 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,12 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 counter = 0
 
 #write a method to tell us how many turns have been played
 def whose_turn(board)
   if #{turn is played} counter += 1 
 end
+
 
 #write a method that tells you whose turn it is
 
